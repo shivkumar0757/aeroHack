f0322684339099f8235def966c3b706b4de1260b
shivkumar0757 <shivkumar0757@gmail.com>
2022-05-26 11:49:13 +0530

    https

--- aeroHack/settings.py
+++ aeroHack/settings.py
@@ -111,4 +111,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['aerothon-proj.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://aerothon-proj.herokuapp.com']
